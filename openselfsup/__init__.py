05f6f646a86c2f18f853467d00b65ce7cf50dabd
Chengxu Zhuang <chengxuz@node06-ccncluster.stanford.edu>
2020-07-22 10:38:32 -0700

    change versions to init

--- openselfsup/__init__.py
+++ openselfsup/__init__.py
@@ -1,3 +1,5 @@
-from .version import __version__, short_version
+#from .version import __version__, short_version
 
+__version__ = '0.2.0+802fbf2'
+short_version = '0.2.0'
 __all__ = ['__version__', 'short_version']
